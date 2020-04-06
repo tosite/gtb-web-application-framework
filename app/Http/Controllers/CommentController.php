165c5f18d0207003a6989accc3033201d92a7120
tosite <n.teshima@pepabo.com>
2020-04-06 19:29:56 +0900

    get-method-mock

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,7 +8,10 @@ class CommentController extends Controller
 {
     public function index()
     {
-        //
+        return [
+            ['id' => 1, 'name' => 'yamada', 'content' => 'hello'],
+            ['id' => 2, 'name' => 'tanaka', 'content' => 'world'],
+        ];
     }
 
     public function store(Request $request)
@@ -18,7 +21,7 @@ class CommentController extends Controller
 
     public function show($id)
     {
-        //
+        return ['id' => 2, 'name' => 'tanaka', 'content' => 'world'];
     }
 
     public function update(Request $request, $id)
