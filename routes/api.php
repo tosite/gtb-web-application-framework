d8ebdbbea8aea68563e752f4eb45185c71d9438c
tosite <n.teshima@pepabo.com>
2020-04-06 18:24:12 +0900

    add route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('comments', 'CommentController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
 });
