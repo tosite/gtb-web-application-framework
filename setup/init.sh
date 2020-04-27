30654c62f81d9be764ff6b527c6fc6d523f7c98b
endu <futoshi.endo@pepabo.com>
2020-04-27 17:26:59 +0900

    初期セットアップの設定ファイルを追加する

---
+++ setup/init.sh
@@ -0,0 +1,22 @@
+### gtb2020-laravelをConohaで使えるよう初期化セットアップします。
+
+# composer install
+composer install
+
+# .envを作成する
+cp ../.env.example .env
+
+# httpd.confを上書きさせる。-bオプションで上書き元はバックアップする
+sudo cp -b -f httpd/conf/httpd.conf /etc/httpd/conf/
+
+# Apache再起動
+service httpd restart
+
+# logにアクセスする為に権限を付与させる
+chown apache:apache /var/www/html/gtb2020-laravel/storage/logs/
+
+# gtb2020-laravelディレクトリの権限を変更させる
+chmod -R 777 /var/www/html/gtb2020-laravel
+
+# APP KEYを生成させる
+php artisan key:generate
